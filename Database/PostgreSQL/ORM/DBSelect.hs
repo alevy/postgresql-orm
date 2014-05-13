d0c2c19e20f0366682bc579f6550da37b0f0f69d
David Terei <code@davidterei.com>
2014-05-12 17:45:20 -0700

    Add dbGroupByA function for executing a dbselect join.

--- Database/PostgreSQL/ORM/DBSelect.hs
+++ Database/PostgreSQL/ORM/DBSelect.hs
@@ -13,6 +13,7 @@ module Database.PostgreSQL.ORM.DBSelect (
   , dbSelectParams, dbSelect
   , Cursor(..), curSelect, curNext
   , dbFold, dbFoldM, dbFoldM_
+  , dbGroupByA
   , renderDBSelect, buildDBSelect
     -- * Creating DBSelects
   , emptyDBSelect, expressionDBSelect
@@ -360,6 +361,17 @@ dbFoldM c act initial dbs = do
 dbFoldM_ :: (MonadIO m, Model model)
          => Connection -> (model -> m ()) -> DBSelect model -> m ()
 dbFoldM_ c act dbs = dbFoldM c (const act) () dbs
+
+-- | Group the returned tuples by unique a's. Expected that the query retuns
+-- the a's in sorted order by ID. Useful for outer joins.
+dbGroupByA :: (Model a, Model b)
+           => Connection -> DBSelect (a :. b) -> IO [(a, [b])]
+dbGroupByA c ab = dbFold c group [] ab
+  where
+    group :: (Model a, Model b) => [(a, [b])] -> (a :. b) -> [(a, [b])]
+    group    []     (a :. b) = [(a, [b])]
+    group ls@(l:_)  (a :. b) | mkDBRef a /= mkDBRef (fst l) = (a, [b]):ls
+    group    (l:ls) (_ :. b) = (fst l, b:(snd l)):ls
 
 -- | Create a join of the 'selFields', 'selFrom', and 'selWhere'
 -- clauses of two 'DBSelect' queries.  Other fields are simply taken
