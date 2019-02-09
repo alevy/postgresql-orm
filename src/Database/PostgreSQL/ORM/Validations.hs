cf09fd4a454aaff005e955730604c250ad04c112
Bogdan Neterebskii <bog2dan1@gmail.com>
2019-02-09 17:42:12 +0300

    version 0.5.1: GHC 8.6 support, stack support

--- src/Database/PostgreSQL/ORM/Validations.hs
+++ src/Database/PostgreSQL/ORM/Validations.hs
@@ -9,16 +9,27 @@ import Data.Monoid
 #endif
 import qualified Data.Text as T
 import Data.Typeable
+#if __GLASGOW_HASKELL >= 806
+import Data.Semigroup
+#endif
 
 newtype ValidationError = ValidationError
   { validationErrors :: H.HashMap T.Text [T.Text] } deriving (Show, Typeable)
 
 instance Exception ValidationError
 
+#if __GLASGOW_HASKELL__ >= 806
+instance Semigroup ValidationError where
+  ein <> zwei = ValidationError $!
+    H.unionWith mappend (validationErrors ein) (validationErrors zwei)
+#endif
+
 instance Monoid ValidationError where
   mempty = ValidationError mempty
+#if __GLASGOW_HASKELL < 806
   mappend ein zwei = ValidationError $!
     H.unionWith mappend (validationErrors ein) (validationErrors zwei)
+#endif
 
 instance ToJSON ValidationError where
   toJSON = toJSON . validationErrors
